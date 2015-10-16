f3ca658f21716b6aae82bee19a621e9e9f05f43a
Eleni Chourouzidou <eleni@webspirit.gr>
2015-10-16 11:27:02 +0200

    Appointment: added area_id, date to strong params

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -69,6 +69,6 @@ class AppointmentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def appointment_params
-      params.require(:appointment).permit(:price, :client_id)
+      params.require(:appointment).permit(:price, :client_id, :area_id, :date)
     end
 end
