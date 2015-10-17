e2523988058f49f36ef34b0ba6da7f2cbaef67d5
Eleni Chourouzidou <eleni@webspirit.gr>
2015-10-17 23:03:02 +0200

    Added styling in client views

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -69,6 +69,6 @@ class ClientsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def client_params
-      params.require(:client).permit(:first_name, :last_name)
+      params.require(:client).permit(:first_name, :last_name, :telephone)
     end
 end
